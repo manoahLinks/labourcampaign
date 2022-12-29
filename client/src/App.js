6cb168a305274169fe6dadb65a91b2c48888627f
manoahLinks <manoahluka@gmail.com>
2022-12-29 22:28:15 +0100

    removing navbar

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,13 @@ import React, { Component } from 'react';
 import {BrowserRouter as Router, Routes, Route  } from 'react-router-dom';
 import LoginPage from './pages/LoginPage'
 import RegisterationPage from './pages/RegistrationPage';
-import NavBar from './components/NavBar';
+
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <div className='grid grid-cols-1 text-xs'>
-      
+        <div className='grid grid-cols-1 text-xs'>   
           <div className='grid grid-cols-1'>
             <Routes>
               <Route exact path={`/`} element={<LoginPage></LoginPage>}></Route>
