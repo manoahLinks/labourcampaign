b32142d0f5579c69bfe91990b1523a8cd484685c
manoahLinks <manoahluka@gmail.com>
2022-12-29 19:13:18 +0100

    reinstalling forms

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.get(/^\/(?!api).*/, function(_, res) {
         function (err) {
             if(err) {
                 res.status(500).send(err)
+                console.log('in')
             }
         }
     )
