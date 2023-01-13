1784c8b192aed6cd1c62de010fbd5ea66656f88e
manoahLinks <manoahluka@gmail.com>
2023-01-13 22:45:45 +0100

    new api end points

--- client/src/hooks/useLogin.js
+++ client/src/hooks/useLogin.js
@@ -11,7 +11,7 @@ export const useLogin = () => {
         setIsLoading(true)
         setError(null)
 
-        const response = await fetch(`login`, {
+        const response = await fetch(`https://objective-mayer.204-93-216-231.plesk.page/login`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
--- client/src/hooks/useSignup.js
+++ client/src/hooks/useSignup.js
@@ -11,7 +11,7 @@ export const useSignup = () => {
         setIsLoading(true)
         setError(null)
 
-        const response = await fetch(`register`, {
+        const response = await fetch(`https://objective-mayer.204-93-216-231.plesk.page/register`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
