8f3a759081b99f3ceae196220f0f60ec1d65def2
manoahLinks <manoahluka@gmail.com>
2023-01-14 12:45:43 +0100

    adding cors-anywhere to handle proxy

--- client/src/hooks/useLogin.js
+++ client/src/hooks/useLogin.js
@@ -11,7 +11,7 @@ export const useLogin = () => {
         setIsLoading(true)
         setError(null)
 
-        const response = await fetch(`https://objective-mayer.204-93-216-231.plesk.page/login`, {
+        const response = await fetch(` https://cors-anywhere-m.herokuapp.com/https://objective-mayer.204-93-216-231.plesk.page/login`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
